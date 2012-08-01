720b08d8fa7e50d9415bf5029a7db45b8ff2a732
Dan Lidral-Porter <dlp@aperiodic.org>
2012-08-01 11:11:57 -0700

    Update quil dep, reflection warnings in lein check

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject protozoa "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [quil "1.4.1"]]
-  :main protozoa.core)
+                 [quil "1.6.0"]]
+  :main protozoa.core
+  :warn-on-reflection true)
