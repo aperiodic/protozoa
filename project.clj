d8710b5526775de566881f9cb1295dc14389f28a
Dan Lidral-Porter <dlp@aperiodic.org>
2012-08-01 02:29:00 -0700

    Add :main to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject protozoa "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [quil "1.4.1"]])
+                 [quil "1.4.1"]]
+  :main protozoa.core)
